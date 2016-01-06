a5b0153ed9f32d011147bd024d092adb6b408b1a
Carlos Lemus <carlosslemus@yahoo.com>
2016-01-06 13:41:46 -0800

    Add ead exporter

---
+++ unlv_ead_exporter/backend/model/ead_serializer_ext.rb
@@ -0,0 +1,250 @@
+require 'date'
+class EADSerializer < ASpaceExport::Serializer
+  serializer_for :ead
+  
+  
+  def self.run_serialize_step(data, xml, fragments, context)
+    Array(@extra_serialize_steps).each do |step|
+      step.new.call(data, xml, fragments, context)
+    end
+  end
+
+  def stream(data)
+    @stream_handler = ASpaceExport::StreamHandler.new
+    @fragments = ASpaceExport::RawXMLHandler.new
+    @include_unpublished = data.include_unpublished?
+    @use_numbered_c_tags = data.use_numbered_c_tags?
+    @id_prefix = I18n.t('archival_object.ref_id_export_prefix', :default => 'aspace_')
+
+    doc = Nokogiri::XML::Builder.new(:encoding => "UTF-8") do |xml|
+      begin
+
+      xml.ead(                  'xmlns:xsi' => 'http://www.w3.org/2001/XMLSchema-instance',
+                 'xsi:schemaLocation' => 'urn:isbn:1-931666-22-9 http://www.loc.gov/ead/ead.xsd',
+                 'xmlns:xlink' => 'http://www.w3.org/1999/xlink') {
+
+        xml.text (
+          @stream_handler.buffer { |xml, new_fragments|
+            serialize_eadheader(data, xml, new_fragments)
+          })
+
+        atts = {:level => data.level, :otherlevel => data.other_level}
+
+        if data.publish === false
+          if @include_unpublished
+            atts[:audience] = 'internal'
+          else
+            return
+          end
+        end
+
+        atts.reject! {|k, v| v.nil?}
+
+        xml.archdesc(atts) {
+
+
+
+          xml.did {
+
+
+            if (val = data.language)
+              xml.langmaterial {
+                xml.language(:langcode => val) {
+                  xml.text I18n.t("enumerations.language_iso639_2.#{val}", :default => val)
+                }
+              }
+            end
+
+            if (val = data.repo.name)
+              xml.repository {
+                xml.corpname { sanitize_mixed_content(val, xml, @fragments) }
+              }
+            end
+
+            if (val = data.title)
+              xml.unittitle  {   sanitize_mixed_content(val, xml, @fragments) }
+            end
+
+            serialize_origination(data, xml, @fragments)
+
+            xml.unitid (0..3).map{|i| data.send("id_#{i}")}.compact.join('-')
+
+            serialize_extents(data, xml, @fragments)
+
+            serialize_dates(data, xml, @fragments)
+
+            serialize_did_notes(data, xml, @fragments)
+
+            data.instances_with_containers.each do |instance|
+              serialize_container(instance, xml, @fragments)
+            end
+
+            EADSerializer.run_serialize_step(data, xml, @fragments, :did)
+
+          }# </did>
+
+          data.digital_objects.each do |dob|
+                serialize_digital_object(dob, xml, @fragments)
+          end
+
+          serialize_nondid_notes(data, xml, @fragments)
+
+          serialize_bibliographies(data, xml, @fragments)
+
+          serialize_indexes(data, xml, @fragments)
+
+          serialize_controlaccess(data, xml, @fragments)
+
+          EADSerializer.run_serialize_step(data, xml, @fragments, :archdesc)
+
+          xml.dsc {
+
+            data.children_indexes.each do |i|
+              xml.text(
+                       @stream_handler.buffer {|xml, new_fragments|
+                         serialize_child(data.get_child(i), xml, new_fragments)
+                       }
+                       )
+            end
+          }
+        }
+      }
+
+    rescue => e
+      xml.text  "ASPACE EXPORT ERROR : YOU HAVE A PROBLEM WITH YOUR EXPORT OF YOUR RESOURCE. THE FOLLOWING INFORMATION MAY HELP:\n
+                MESSAGE: #{e.message.inspect}  \n
+                TRACE: #{e.backtrace.inspect} \n "
+    end
+
+
+
+    end
+    doc.doc.root.add_namespace nil, 'urn:isbn:1-931666-22-9'
+
+    Enumerator.new do |y|
+      @stream_handler.stream_out(doc, @fragments, y)
+    end
+  end
+  def serialize_eadheader(data, xml, fragments)
+    eadheader_atts = {:findaidstatus => data.finding_aid_status,
+                      :repositoryencoding => "iso15511",
+                      :countryencoding => "iso3166-1",
+                      :dateencoding => "iso8601",
+                      :langencoding => "iso639-2b"}.reject{|k,v| v.nil? || v.empty? || v == "null"}
+
+    xml.eadheader(eadheader_atts) {
+
+      eadid_atts = {:countrycode => data.repo.country,
+              :url => data.ead_location,
+              :mainagencycode => data.mainagencycode}.reject{|k,v| v.nil? || v.empty? || v == "null" }
+
+      xml.eadid(eadid_atts) {
+        xml.text data.ead_id
+      }
+
+      xml.filedesc {
+
+        xml.titlestmt {
+
+          titleproper = ""
+          titleproper += "#{data.finding_aid_title} " if data.finding_aid_title
+          titleproper += "#{data.title}" if ( data.title && titleproper.empty? )
+          xml.titleproper("type" => "filing") { sanitize_mixed_content(data.finding_aid_filing_title, xml, fragments)} unless data.finding_aid_filing_title.nil?
+          xml.titleproper {  sanitize_mixed_content(titleproper, xml, fragments) }
+          xml.subtitle {  sanitize_mixed_content(data.finding_aid_subtitle, xml, fragments) } unless data.finding_aid_subtitle.nil?
+          xml.author { sanitize_mixed_content(data.finding_aid_author, xml, fragments) }  unless data.finding_aid_author.nil?
+          xml.sponsor { sanitize_mixed_content( data.finding_aid_sponsor, xml, fragments) } unless data.finding_aid_sponsor.nil?
+
+        }
+
+        unless data.finding_aid_edition_statement.nil?
+          xml.editionstmt {
+            sanitize_mixed_content(data.finding_aid_edition_statement, xml, fragments, true )
+          }
+        end
+
+        xml.publicationstmt {
+	
+		  val = Date.today.strftime("%Y") + " The Regents of the University of Nevada. All rights reserved."
+		  xml.publisher { sanitize_mixed_content(val, xml, fragments) }
+          xml.publisher("altrender" => "bold") { sanitize_mixed_content(data.repo.name.strip ,xml, fragments) }
+
+          if data.repo.image_url
+            xml.p ( { "id" => "logostmt" } ) {
+              xml.extref ({"xlink:href" => data.repo.image_url,
+                          "xlink:actuate" => "onLoad",
+                          "xlink:show" => "embed",
+                          "xlink:type" => "simple"
+                          })
+                          }
+          end
+          if (data.finding_aid_date)
+            xml.p {
+                  val = data.finding_aid_date
+                  xml.date {   sanitize_mixed_content( val, xml, fragments) }
+                  }
+          end
+
+          unless data.addresslines.empty?
+            xml.address {
+              data.addresslines.each do |line|
+                xml.addressline { sanitize_mixed_content( line, xml, fragments) }
+              end
+              if data.repo.url
+                xml.addressline ( "URL: " ) {
+                  xml.extptr ( {
+                          "xlink:href" => data.repo.url,
+                          "xlink:title" => data.repo.url,
+                          "xlink:type" => "simple",
+                          "xlink:show" => "new"
+                          } )
+                 }
+              end
+            }
+          end
+        }
+
+        if (data.finding_aid_series_statement)
+          val = data.finding_aid_series_statement
+          xml.seriesstmt {
+            sanitize_mixed_content(  val, xml, fragments, true )
+          }
+        end
+        if ( data.finding_aid_note )
+            val = data.finding_aid_note
+            xml.notestmt { xml.note { sanitize_mixed_content(  val, xml, fragments, true )} }
+        end
+
+      }
+
+      xml.profiledesc {
+        creation = "This finding aid was produced using ArchivesSpace on <date>#{Time.now}</date>."
+        xml.creation {  sanitize_mixed_content( creation, xml, fragments) }
+
+        if (val = data.finding_aid_language)
+          xml.langusage (fragments << val)
+        end
+
+        if (val = data.descrules)
+          xml.descrules { sanitize_mixed_content(val, xml, fragments) }
+        end
+      }
+
+      if data.revision_statements.length > 0
+        xml.revisiondesc {
+          data.revision_statements.each do |rs|
+              if rs['description'] && rs['description'].strip.start_with?('<')
+                xml.text (fragments << rs['description'] )
+              else
+                xml.change {
+                  rev_date = rs['date'] ? rs['date'] : ""
+                  xml.date (fragments <<  rev_date )
+                  xml.item (fragments << rs['description']) if rs['description']
+                }
+              end
+          end
+        }
+      end
+    }
+  end
+end
