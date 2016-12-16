205f7ebb8bb98e63bdab21f14650d3a1baa538e8
Carlos Lemus <carlosslemus@yahoo.com>
2016-12-16 10:43:34 -0800

    Add Marc Export changes
    Add remove tag 254, 008 , and Agent notes

--- unlv_marc_exporter/schemas/m_export_settings.rb
+++ unlv_marc_exporter/schemas/m_export_settings.rb
@@ -6,26 +6,29 @@
     "properties" => {	
 
 		#Enable Tags 
-		"tag_041" => {"type" => "boolean", "default" => true},	
+		"tag_008" => {"type" => "boolean", "default" => false},	
+		"tag_041" => {"type" => "boolean", "default" => false},	
 		"tag_099" => {"type" => "boolean", "default" => true},	
-		"tag_245_sc_f" => {"type" => "boolean",  "default" => true},
-		"tag_351" => {"type" => "boolean", "default" => true},
-		"tag_500" => {"type" => "boolean", "default" => true},	
+		"tag_245_sc_f" => {"type" => "boolean",  "default" => false},
+		"tag_245_sc_g" => {"type" => "boolean",  "default" => false},
+		"tag_351" => {"type" => "boolean", "default" => false},
+		"tag_500" => {"type" => "boolean", "default" => false},	
 		"tag_506" => {"type" => "boolean", "default" => true},	
-		"tag_520_ind1_3" => {"type" => "boolean", "default" => true},
-		"tag_541" => {"type" => "boolean", "default" => true},	
+		"tag_520_ind1_3" => {"type" => "boolean", "default" => false},
+		"tag_541" => {"type" => "boolean", "default" => false},	
 		"tag_555" => {"type" => "boolean", "default" => true},	
-		"tag_852" => {"type" => "boolean", "default" => true},	
+		"tag_852" => {"type" => "boolean", "default" => false},	
 		"tag_856" => {"type" => "boolean", "default" => true},	
 		
 		#Special Settings
-		"tag_ss_1" => {"type" => "boolean", "default" => true},
-		"tag_ss_2" => {"type" => "boolean", "default" => true},
-		"tag_506_sc_a_ss_1" => {"type" => "boolean", "default" => true},
-		"tag_ss_3" => {"type" => "string", "required" => false},
-		"tag_555_ss_1" => {"type" => "string", "required" => false},
-		"tag_856_ss_1" => {"type" => "string", "required" => false},
-		"tag_610_sc_a_ss_1" => {"type" => "boolean", "default" => true},
+		"tag_ss_1" => {"type" => "boolean", "default" => true}, #strip hard returns
+		"tag_ss_2" => {"type" => "boolean", "default" => true}, #replace period with dash identifier
+		"tag_610_sc_a_ss_1" => {"type" => "boolean", "default" => true}, #unique qualifier with name for Tag 610
+		"tag_506_sc_a_ss_1" => {"type" => "boolean", "default" => true}, #Search for url in Tag 506 and create additional Tag 856
+		"tag_ss_4" => {"type" => "boolean", "default" => false}, #Add agent notes 
+		"tag_ss_3" => {"type" => "string", "required" => false}, #Change label for created Tag 856
+		"tag_555_ss_1" => {"type" => "string", "required" => false}, #Change label for finding aid content Tag 555
+		"tag_856_ss_1" => {"type" => "string", "required" => false}, #Change label for finding aid content Tag 856
 	
 	},
   },
