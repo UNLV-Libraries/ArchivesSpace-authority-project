8136d2f46aa15472dbb6da115227e1beb9d90437
Seth Shaw <seth.shaw@unlv.edu>
2017-12-06 15:05:50 -0800

    update plugin_init.rb for 2.2
    ArchivesSpace 2.2 uses a method for adding routes instead of concat.

--- unlv_spawn/frontend/plugin_init.rb
+++ unlv_spawn/frontend/plugin_init.rb
@@ -1,5 +1,4 @@
-my_routes = [File.join(File.dirname(__FILE__), "routes.rb")]
-ArchivesSpace::Application.config.paths['config/routes'].concat(my_routes)
+ArchivesSpace::Application.extend_aspace_routes(File.join(File.dirname(__FILE__), "routes.rb"))
 
 Rails.application.config.after_initialize do
 
